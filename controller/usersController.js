7e01ca51ce6dfb1ffadd023ec95f7f3921f1c0b8
hlukman86 <106459212+hlukman86@users.noreply.github.com>
2022-08-07 13:01:34 +0700

    add users

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,13 +1,62 @@
-const User = require("../model/Users.js")
+const Users = require("../model/Users.js")
 
 module.exports = {
-    getAllUser: async function(req,res) {
+    getAllUsers: async function(req,res) {
          
         try {
-            const result = await User.get(req,res)
-            return res.send(result)
+            const result = await Users.get(req,res)
+            return res.status(200).send({
+                result,
+                message: 'succes'
+            })
         } catch(err) {
-            res.status(500).send(err)
+            return res.status(500).send(err)
+        }
+    },
+
+    getUsersId: async (req, res) => {
+        try {
+          const results = await Users.getId(req, res);
+          res.status(200).send(results);
+        } catch (error) {
+          res.send(error);
+        }
+      },
+
+    addNewUsers: async (req, res)=> {
+        console.log(req.file, 'filename dari upload')
+        try {
+            console.log({...req.body, image: 'oke'})
+            const reqModifer = {
+                ...req,
+                body: { ...req.body, image: req.file.filename }
+            }
+            const results = await Users
+            .add(reqModifer, res)
+            return res.status(201).send(results)
+        } catch (error) {
+            return res.status(400).send(error)
+        }
+    },
+
+    updateUsers: async(req, res) => {
+        try {
+            if(req.file){
+                reqModifer = {
+                    ...req,
+                    body: { ...req.body, image: req.file.filename }
+                } 
+            } else {
+                reqModifer = {
+                    ...req,
+                    body: { ...req.body}
+                }
+            }
+            const results = await Users
+            .update(reqModifer, res)
+            return res.status(201).send(results)
+        } catch (error) {
+            return res.status(400).send(error)
         }
     },
 
